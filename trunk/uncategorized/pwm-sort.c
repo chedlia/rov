9e12798c607f5b0ae6f9564ea41669bfba4d8928
da1nerd <da1nerd@3ede6e2e-0ac0-11de-b17c-0f1ef23b492c>
2009-03-24 21:51:06 +0000

    moved most of the code into functions and added some microchipspecific stuff

--- trunk/uncategorized/pwm-sort.c
+++ trunk/uncategorized/pwm-sort.c
@@ -1,77 +1,71 @@
 #import <stdio.h>
 #import <stdlib.h>
 
-const int NUM_MOTORS = 4;
-
-typedef struct motor_info {
-	int data;
-	//other stuff
-} motor_data;
-
-/*
-
 #include "16F877A.h"
 #device ADC=8
- 
 #use delay(clock=8000000)
 
-const int NUM_MOTORS = 3;
+const unsigned int NUM_MOTORS = 3;
 
-typedef enum _MOTOR_STATE
-{
-	UNDEFINED,
-	SET_HIGH,
-	SET_LOW
-} MOTOR_STATE, *PMOTOR_STATE;
+typedef struct motor_info {
+	unsigned int data;
+	unsigned int pina;
+	unsigned int pinb;
+	//unsigned int ontime; //is this needed?
+	//other stuff?
+} motor_data;
 
-typedef struct _MOTOR
-{
-	unsigned int vin;
+static void sort(motor_data *motor[]);
+static void initilize(motor_data *motor[]);
+static void readADC(motor_data *motor[]);
 
-	unsigned int reverse;
-	unsigned int rev_pin;
-	MOTOR_STATE prev_rev_state;
-
-	unsigned int forward;
-	unsigned int fwd_pin;
-	MOTOR_STATE prev_fwd_state;
-} MOTOR, *PMOTOR;
-*/
-
-
-
-int main(){
-	int len = NUM_MOTORS;
-	motor_data *myArray[len];
-	motor_data *swap;
-	int x = 0;
+main(){
+	motor_data *motor[NUM_MOTORS];
+	initilize(motor);
 	
-	for (x = len-1; x >= 0; x--){ //initilize and set some values
-		myArray[x] = malloc(sizeof(motor_data));
-		(*myArray[x]).data = x*-1;
+	while(1){
+		readADC(motor);
+		sort(motor);
 	}
+}
+
+//sorts the array smallest to largest
+void sort(motor_data *motor[]){
+	int x = 0;
+	motor_data *swap;
 	
-	
-	printf("array before sort:\n");
-	for (x = 0; x < len; x++)
-		printf("%i\n", (*myArray[x]).data);
-	
-	x = 0;
-	while(++x<len){ //sort za array!
-		if((*myArray[x-1]).data > (*myArray[x]).data){
-			swap = myArray[x];
-			myArray[x] = myArray[x-1];
-			myArray[x-1] = swap;
+	while(++x<NUM_MOTORS){ //sort the array
+		if((*motor[x-1]).data > (*motor[x]).data){ 
+			swap = motor[x];
+			motor[x] = motor[x-1];
+			motor[x-1] = swap;
 			x = 0;
 		}
 	}
+}
+
+//initilize	the constants
+void initilize(motor_data *motor[]){
+	int x = 0;
 	
-	printf("\narray after sort:\n");
-	for (x = 0; x < len; x++)
-		printf("%i\n", (*myArray[x]).data);
-		
-	for (x = 0; x < len; x++)
-		free(myArray[x]); //free up memory so there are no memory leaks
-		
-	return 0;
+	for (x = 0; x < NUM_MOTORS; x++) //initilize and set some values
+		motor[x] = malloc(sizeof(motor_data));
+	(*motor[0]).pina = PIN_B0;
+	(*motor[0]).pinb = PIN_B1;
+	
+	(*motor[1]).pina = PIN_B2;
+	(*motor[1]).pinb = PIN_B3;
+	
+	(*motor[2]).pina = PIN_B4;
+	(*motor[2]).pinb = PIN_B5;
+}
+
+static void readADC(motor_data *motor[]){
+	//in this case ADC channels are 0,1,2... may not be in other microcontrollers
+	int channel;
+	for(channel = 0; channel < NUM_MOTORS; channel++){
+		set_adc_channel(channel);
+		(*motor[channel]).data = read_adc();
+		delay_ms(10); //need to delay a little to get a good reading
+	}
 }
