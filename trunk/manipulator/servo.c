fbefadcde0d53c2cba05b988c91473bba94cd0fe
da1nerd <da1nerd@gmail.com>
2009-03-28 05:21:02 +0000

    this should control all 8 servos once we figure how to put Matt's [channel] array into line 37

---
+++ trunk/manipulator/servo.c
@@ -0,0 +1,39 @@
+#include "16F877A.h"                                  //identify pic
+#device ADC=8    
+#use delay(clock=8000000)                                      //8 bit analog capturevoid main()
+unsigned int read;
+int step;
+int channel;
+int oldpot[8];
+
+void setspeed (void);
+void main()
+{
+	setup_adc(adc_clock_internal);
+	setup_adc_ports(ALL_ANALOG); 
+	for(;;)
+	{
+		for(channel = 0; channel <=8; channel++)
+		{
+			
+			set_adc_channel(0);
+			delay_us(10);
+			read=read_adc();
+			if (read!=oldpot[channel])
+				setspeed();
+		}
+	}
+}
+void setspeed(void)
+{
+	read=read/4;
+	output_high(PIN_B0);
+	delay_us(105);      
+	for(step=0;step<30;++step)
+	{
+		if (step>read)
+            output_low(PIN_B0);//  NEED HELP HERE!!!        
+	}
+	oldpot[channel]=read;
+      delay_ms(.5);
+}
