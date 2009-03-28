94006745ccd6b493c257079912131f839f3187a1
da1nerd <da1nerd@gmail.com>
2009-03-28 18:27:10 +0000

    Not sure about the exact changes Matt made to this. He has failed to put any comments in the code *frown*. Things seem to be cleaned up a little and more concise.

--- trunk/manipulator/servo.c
+++ trunk/manipulator/servo.c
@@ -1,40 +1,38 @@
-//This will eventually control all 8 servos
-#include "16F877A.h"                                  //identify pic
-#device ADC=8    
-#use delay(clock=8000000)                                      //8 bit analog capturevoid main()
-unsigned int read;
-int step;
-int channel;
-int oldpot[8];
+//Yes I know there are no comments!!!  It is almost 3:00am
+//for crying out loud!!!
 
+#include "16F877A.h"                                 //identify pic
+#device ADC=10    
+#use delay(clock=8000000)                                      //8 bit analog capturevoid main()
+#include <math.h> 
+   int16 read;
+   int channel;
+   int oldpot[8];
+   int out;
 void setspeed (void);
 void main()
 {
-	setup_adc(adc_clock_internal);
-	setup_adc_ports(ALL_ANALOG); 
-	for(;;)
-	{
-		for(channel = 0; channel <=8; channel++)
-		{
-			
-			set_adc_channel(0);
-			delay_us(10);
-			read=read_adc();
-			if (read!=oldpot[channel])
-				setspeed();
-		}
-	}
+   setup_adc(adc_clock_internal);
+   setup_adc_ports(ALL_ANALOG); 
+   for(;;)
+   {
+      for(channel = 0; channel <=8; channel++)
+      {         
+         set_adc_channel(channel);
+         delay_us(10);
+         read=read_adc();
+         if (read!=oldpot[channel])
+            setspeed();
+      }
+   }
 }
 void setspeed(void)
 {
-	read=read/4;
-	output_high(PIN_B0);
-	delay_us(105);      
-	for(step=0;step<30;++step)
-	{
-		if (step>read)
-            output_low(PIN_B0);//  NEED HELP HERE!!!        
-	}
-	oldpot[channel]=read;
-      delay_ms(.5);
+      out=pow(2,(channel));
+      output_B(out);
+      delay_us(989);      
+      delay_us(read);
+      output_B(0);
+      oldpot[channel]=read;
+      delay_ms(18);
 }
