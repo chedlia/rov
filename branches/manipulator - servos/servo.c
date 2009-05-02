4fde57f63bde6d7cafdab8bec9b5586362a088ab
da1nerd@gmail.com <da1nerd@gmail.com@3ede6e2e-0ac0-11de-b17c-0f1ef23b492c>
2009-05-02 05:20:33 +0000

    We've decided to use stepper motors in the manipulator, so development on the servo code is currently dead.

---
+++ branches/manipulator - servos/servo.c
@@ -0,0 +1,44 @@
+/*
+This program controls 8 servos with a pic and 8 potentiometers
+*/
+
+#include "16F877A.h"			/*identify pic*/
+#device ADC=10
+#use delay(clock=8000000)       /*8 bit analog capture void main()*/
+#include <math.h>
+   int16 read;
+   int channel;
+   int oldpot[8];
+   int out;
+
+/*initialize functions*/
+void setspeed (void);
+
+void main()
+{
+	setup_adc(adc_clock_internal);      /* setup the internal clock*/
+	setup_adc_ports(ALL_ANALOG);        /* tell pic we want bank A to be analog*/
+	while(1)
+	{
+	for(channel = 0; channel <=8; channel++)   /* loop through all our chanels and check for changes*/
+		{
+			set_adc_channel(channel);     /* set which chanel to read from*/
+			delay_us(20);                 /* give it some time to think. note: we get warnings if shorter delay*/
+			read=read_adc();              /* reads input to the chip*/
+			if (read!=oldpot[channel])    /* set the speed if we detect a change*/
+				setspeed();
+		}
+	}
+}
+
+/* Set the speed and angle of servo*/
+void setspeed(void)
+{
+		oldpot[channel]=read;      /* keep track of the current chanels position*/
+		read=read + 989;           /* delay 1 ms*/
+		out=pow(2,(channel));      /* convert chanel to binary*/
+		output_B(out);             /* turn on*/
+		delay_us(read);            /* give a pulse to set the angle*/
+		output_B(0);               /* turn off all chanels*/
+		/* delay_ms(18);*/
+}
