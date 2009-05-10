ecca27cfad8827c1bad7442675ea4a8323dc9b44
da1nerd@gmail.com <da1nerd@gmail.com@3ede6e2e-0ac0-11de-b17c-0f1ef23b492c>
2009-05-10 08:26:38 +0000

    This is the first major release of the manipulator code created by Matt

--- trunk/manipulator/manipulator.c
+++ trunk/manipulator/manipulator.c
@@ -1,90 +1,68 @@
-/*
-manipulator.c
-Intended to control the motion of the manipulator arm(s) attached to the ROV.
- */
+/*   manipulator.c
+    8 stepper Motor Control with 16F877A   
+*********************************************/
 
-/* 
- NOTES: 
- Please place any general notes here. What is a general note? A note that may be needed to further explain why you chose to do something
- and to keep the rest of the code organized and concise. you may also offer ideas.
- Please place your name and time stamp by any contributed note.
- 
- Arm location tracker. - Joel Lonbeck February 15, 2009	9:36 PM	
- After some thought it seems nessesary to keep track of the manipulator's location as it moves. 
- unless there is some reading we can take from the hardware that would allow us to determine it's current location.
- Tracking the manipulator location would have several benefits:
- 1. use macros - ie. return to default position, extend arm full length.
- 2. visualization of current arm position - ie. a display on the controller that might state or display the current arm position.
- The tracker may be needed to calculate the movement nessesary to place the manipulator in the desired posture. 
- I've placed a proposed skeleton function below.
- 
- Standard representation of movement.  - Joel Lonbeck February 15, 2009	9:54 PM	
- I think we need to decide on a standard representation of movement for the manipulators (and everything else).
- for example: "1" equals 1 unit of positive movement, 
- and -5 equals 5 units of negative movement. We will also need to decide which way will be positive/negative.
- */	
+#include "16F877A.h"                                      /* identify pic */
+#device ADC=8                                             /* 8 bit analog capture*/
+#fuses XT, PUT, NOWDT, NOPROTECT, BROWNOUT, NOCPD         /* enable nominal speed external clock, enable power up timer
+                                                          /* disable watchdog timer, program memory protect disabled,
+                                                          /* brownout detect/reset enabled, EEPROM data memory protect disabled */
+#use delay(clock=4000000)                                 /* chip set at 4Mhz*/
+#include "math.h" 
 
-#include "../header/ROV.h"
-
-/*This struct is used to keep track of the manipulators position. */
-
-struct Manip
-{
-	int joint[5];
-	int gripper;
-};
-
-/*
- MOVEJOINT
- a unversal function telling each motor how to move
- @param motor; 1,2,3 etc.
- @param direction; FORWARD, REVERSE
- @param step; how many steps it should make?
- */
-void movejoint(int motor, int direction, int step)
+void main()
 {
 
-}
+   unsigned int vin[8];                                   /* vin [0,1,2] varibles the potentiometer values are stored into */
+   unsigned int channel;                                  /* variable for voltage in loop */
+   unsigned int location[8];                              /* variable to set the speed of motor 1,2,3 */
+   unsigned int work;                                     /* binary output on bank B variable */
+   unsigned int direction;
+   unsigned int position[8];
+   int step;                                              /* The step variable is used to count through the duty cycle */
+                                     
+   setup_adc(ADC_CLOCK_INTERNAL);                         /*  Setup ADC input*/
+   setup_adc_ports(ALL_ANALOG);
 
-/*
- GRIPSTATE
- controls the gripper's (at the end of the arm) on/off state
- @param state; OFF, ON
- */
-void setGripState(int state)
-{	
-
-}
-
-/*
- RESET
- returns the manipulator to the default position.
- */
-void reset()
-{
-
-}
-
-/*
-track
- tracks the movement of the manipulator. This function may be called by movement functions in order to keep the current location of the
- manipulator up to date. The input of the parameters are still unclear. They will need to convey the proposed movement of the manipulator.
- We will need to decide on a standard repesentation of movement.
- @param joint[]
- @param gripper
- */
-void track(int joint[], int gripper)
-{
-
-}
-
-/*
-getState
- returns the current state of the manipulator. ie. motor1 is at position 3, motor2 is at position 5, motor3 is at position 1, etc
- @return manip; returns the manip struct
- */
-
-Manip getState()
-{
-	return Manip;
+   while(true)                                            /* infinite loop */
+   {
+      direction = 0;
+      work = 0;                                           /* reset the work variable */
+      for(channel = 0; channel <= 7; channel++){          /* loop through the input channels */
+         set_adc_channel(channel);                        /* setup channel based on loop */
+         vin[channel] = read_adc();                       /* read the input of the channel */
+         delay_us(10);                                    /* small delay to give the pic time to read the input value */
+         //vin[channel]*multiplyer to set position?
+         
+         if (vin[channel] == position[channel])           /* if the position is equal the the input then stay there*/
+            location[channel] = vin[channel];
+            
+         else if (vin[channel] > position[channel])       /* if the input is greater than the position then move forwards to that position*/
+            location[channel] = vin[channel] - position[channel];
+    
+         else if (vin[channel] < position[channel])  {    /* if the input is less than the position then turn on reverse and go to that position*/
+            location[channel] = position[channel] - vin[channel];
+            direction += pow(2,channel);
+         }
+      }
+      for(step = 0; step < 100; step++)                    /*  For loop where on states occur*/
+         {
+            for(channel = 0; channel <= 7; channel++){     /* loop through the channels*/
+            
+               if (position[channel] == location[channel]) /* if the position is in the correct location turn of that pin */
+                 work -= pow(2,channel);                    
+                 
+               else if (position[channel] > location[channel])/* if the position is beyond the location go down one step*/
+                  position[channel] += 1;
+                  
+               else if (position[channel]<location[channel])/* if the position is before the location move one step*/
+                  position[channel] -= 1;
+            }
+            output_B(direction);                            /* turn on outputs on Bank B based on binary */
+            output_D(work);                                 /* turn on outputs on bank D based on binary */
+            delay_ms (20);                                  /* waits so a pulse can be felt by the stepper*/
+            output_D(0);                                    /* turn off all pins on bank D*/
+            delay_ms (2);                                   /* needs tweeking should equal previous delay with time spent on statements above*/
+         }
+   }
 }
