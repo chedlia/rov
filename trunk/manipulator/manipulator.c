28fb5e2c51117d09693c04c2695b3a67e55f8a52
da1nerd@gmail.com <da1nerd@gmail.com@3ede6e2e-0ac0-11de-b17c-0f1ef23b492c>
2009-06-22 05:09:36 +0000

    we've finally got it working!!

--- trunk/manipulator/manipulator.c
+++ trunk/manipulator/manipulator.c
@@ -9,7 +9,7 @@
 #use delay(clock=4000000)                                 /* chip set at 4Mhz*/
 #include "math.h" 
 
-unsigned int Pot_pos[8];                                   /* vin [0,1,2] varibles the potentiometer values are stored into */
+unsigned int Pot_pos[7];                                   /* vin [0,1,2] varibles the potentiometer values are stored into */
 unsigned int channel;                                      /* variable for voltage in loop */
 unsigned int16 Cur_Loc[7];                                 /* variable to set the speed of motor 1,2,3 */
 unsigned int Status[7];
@@ -25,7 +25,8 @@ void main()
 {
    setup_adc(ADC_CLOCK_INTERNAL);                         /*  Setup ADC input*/
    setup_adc_ports(ALL_ANALOG);
-
+   
+   /* begin control*/
    while(true)                                            /* infinite loop */
    {
       direction = 0;
@@ -36,42 +37,46 @@ void main()
       delay_us(20);
       */
       
-      for (channel =0;channel<7;channel++) {
-         set_adc_channel(channel);                        /* setup channel based on loop */
+      /*initialize channels and read data*/
+      for(channel = 0; channel < 7; channel++) {
+         set_adc_channel(channel);
          delay_us(20);
-         Pot_Pos[channel]= read_adc();                    /* read the input of the channel */
-         delay_us(10);                                    /* small delay to give the pic time to read the input value */
-         Next_Loc[channel]=Pot_Pos[channel]-Cur_Loc[channel];
-         status[channel]=1;
-         if (Next_Loc[channel]<0)
-            direction=direction+pow(2,channel);
-         else 
-            direction=direction-pow(2,channel);
+         Pot_Pos[channel] = read_adc();                        /* read the input of the channel */
+         delay_us(10);                                         /* small delay to give the pic time to read the input value */
+         Next_Loc[channel] = Pot_Pos[channel] - Cur_Loc[channel]; /* calculate the next location*/
+         status[channel] = 1;                                 /* status is 1 till visited*/
+         
+         if(Next_Loc[channel] < 0)
+            direction += pow(2,channel);
+      //   else 
+        //    direction -= pow(2,channel);
       }
       /*
          If (Pot_Pos[7]!= servo_pos)
             servo();
-            */
+      */
+      /* Send pulses*/
       for(step = 0; step < 100; step++){                     /*  For loop where on states occur*/           
-         for (channel =0;channel<8;channel++) {
-            if (Pot_Pos[channel]==Cur_Loc[channel]){
-               if (status[channel]==1) {
-               movement =movement-pow(2,channel);
-               status[channel]=status[channel]-1;  }
-            }                 
-         if (Pot_Pos[channel]>Cur_Loc[channel])
-            Cur_Loc[channel]=Cur_Loc[channel]+1;
-         if (Pot_Pos[channel]<Cur_Loc[channel])
-            Cur_loc[channel]=Cur_Loc[channel]-1;
+         for(channel = 0; channel < 7; channel++) {
+            if(Pot_Pos[channel] == Cur_Loc[channel]){
+               if(status[channel] == 1) {
+                  movement -= pow(2,channel);
+                  status[channel] = status[channel] - 1;  
+               }
+            }
+            if (Pot_Pos[channel] > Cur_Loc[channel])
+               Cur_Loc[channel] = Cur_Loc[channel] + 1;
+            if (Pot_Pos[channel] < Cur_Loc[channel])
+               Cur_loc[channel] = Cur_Loc[channel] - 1;
          }
          output_B(direction);                            /* turn on outputs on Bank B based on binary */
          output_D(movement);                             /* turn on outputs on bank D based on binary */
-         delay_us (20);                                  /* waits so a pulse can be felt by the stepper*/
+         delay_us(20);                                  /* waits so a pulse can be felt by the stepper*/
          output_D(0);                                    /* turn off all pins on bank D*/
          output_b(0);
-         delay_us (2);                                   /* needs tweeking should equal previous delay with time spent on statements above*/
+         delay_us(2);                                   /* needs tweeking should equal previous delay with time spent on statements above*/
+      }
    }
- }
 }  
 /*
 we determined we can't control both the servo and the steppers with the same microcontroller.
